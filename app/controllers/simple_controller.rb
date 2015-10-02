aa14980746f2b9354a957ea120bd7c3668169557
Phitherek_ <phitherek@gmail.com>
2015-10-02 09:33:04 +0200

    Simple logging form.

--- app/controllers/simple_controller.rb
+++ app/controllers/simple_controller.rb
@@ -4,5 +4,13 @@ class SimpleController < ApplicationController
     end
 
     def logging
+        @entries = @user.regular_log_entries.limit(20)
+        @new_entry = RegularLogEntry.new
+    end
+
+    def create
+    end
+
+    def query
     end
 end
